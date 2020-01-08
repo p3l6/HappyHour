26b7e6e8dbf63978eb780d14eba3df722fdbcd58
Paul Landers <planders@statestitle.com>
2020-01-08 10:44:47 -0500

    Initial Commit

---
+++ HappyHour/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HappyHour
+//
+//  Created by Paul Landers on 1/8/20.
+//  Copyright © 2020 Paul Landers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
