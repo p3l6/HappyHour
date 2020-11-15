1d7d9ee4196208faf1ef87fb6812a8cf68a0107b
Paul Landers <planders@statestitle.com>
2020-11-15 10:48:06 -0500

    Delete comments, seems to work without it

--- HappyHour/HappyHourApp.swift
+++ HappyHour/HappyHourApp.swift
@@ -1,6 +1,5 @@
 
 import SwiftUI
-import UserNotifications
 
 @main
 struct HappyHourApp: App {
@@ -19,35 +18,6 @@ struct HappyHourApp: App {
             SettingsView()
                 .environmentObject(settings)
         }
-//        WKNotificationScene {
-//            let notificationCenter = UNUserNotificationCenter.current()
-//        let options: UNAuthorizationOptions = [.alert, .sound]
-//        notificationCenter.requestAuthorization(options: options) {
-//            (didAllow, error) in
-//            if !didAllow {
-//                print("User has declined notifications")
-//            }
-//        }
-//        notificationCenter.delegate = self
-
-//    @objc func showWindow() {
-//        window.makeKeyAndOrderFront(nil)
-//        NSApp.activate(ignoringOtherApps: true)
-//    }
-//
-
-
-//extension AppDelegate: UNUserNotificationCenterDelegate {
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-//        self.showWindow()
-//        completionHandler()
-//    }
-//
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-//        completionHandler([.alert, .sound])
-//    }
-            //}
-//        }
     }
 }
 
